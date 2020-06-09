31c6a30d5baa63b2c1695c125d27e39e0b76a834
andersonsvargas <anderson.svargas89@gmail.com>
2020-06-09 02:19:40 -0300

    git add

--- Js/Main.js
+++ Js/Main.js
@@ -9,8 +9,9 @@ function consultaCep(){
       console.log(response);
      document.getElementById("logradouro").innerHTML =
       response.logradouro; 
-     document.getElementById
-     ("bairro").innerHTML = response.bairro;
+    document.getElementById("bairro").innerHTML = response.bairro;
+     document.getElementById("localidade").innerHTML = response.localidade;
+     document.getElementById("uf").innerHTML = response.uf;
     }
     })
 }
